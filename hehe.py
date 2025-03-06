e7a414838d79324f3a5e560ee574b71f7a704be5
DJ_Theron <97227143+DJTheron@users.noreply.github.com>
2025-03-06 13:40:19 +0200

    Create hehe.py

---
+++ hehe.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from time import sleep
+
+print("This is a virus")
+sleep(2)
+print("Ur cooked like tim")
+sleep(0.5)
+while True:
+        print("rehe")
+                       
